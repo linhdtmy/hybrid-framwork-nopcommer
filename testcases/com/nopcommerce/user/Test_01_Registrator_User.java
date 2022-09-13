be345384d624436b443e2f29d5a7c2f39b6bdd18
dangthimylinh <linhboure@gmail.com>
2022-09-13 23:28:11 +0700

    Create new Project

---
+++ testcases/com/nopcommerce/user/Test_01_Registrator_User.java
@@ -0,0 +1,19 @@
+package com.nopcommerce.user;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class Test_01_Registrator_User {
+  
+  @BeforeClass
+  public void beforeClass() {
+  }
+  @Test
+  public void TC_01() {
+  }
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
---
+++ testdata/com/nopcommerce/data/User_Data.java
@@ -0,0 +1,12 @@
+package com.nopcommerce.data;
+
+public class User_Data {
+
+	public class Register {
+
+	}
+
+	public class MyAccount {
+
+	}
+}
